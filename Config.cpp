d36ab38f04da24262cb93d8f1f88c523df4e8f73
Bill <crupib@gmail.com>
2014-08-22 15:13:58 -0400

    NA

--- Config.cpp
+++ Config.cpp
@@ -12,3 +12,8 @@ void Config_intx(int_state stat, int interupt)
 {
 	printf("Set int%i To be implented in hardware\n",interupt);
 }
+//Config Timer1 = Pwm, Pwm = 8, Compare A Pwm = Clear Down, Compare B Pwm = Clear Down, Prescale = 1       '39.0622Khz pwm
+void Config_timer1()
+{
+
+}
